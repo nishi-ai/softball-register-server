b1eafbea3c7e8b47e78c8de95083def0f23f5f96
Ai Nishikawa <60405643+nishi-ai@users.noreply.github.com>
2021-09-04 09:57:20 +0200

    return array of objects

--- controllers/playersIndexController.js
+++ controllers/playersIndexController.js
@@ -1,9 +1,29 @@
 const db = require('../db');
 
-// GET
+// GET http://localhost:7000/admin/players
 exports.getIndex = (req, res, next) => {
     console.log("GET: Hello getPlayersPage")
-    res.json({
-        message: 'Hello all players'
-    })
+    // connect to the database and save the new incoming player
+    db.getDb()
+        .db('softball')
+        // the collction will be created dynamically if it does not exist yet
+        .collection('players')
+        .find(
+            {},
+            {_id: 0, name: 1, email: 1}
+        )
+        .toArray(
+            function(err, result) {
+                if (err) throw err;
+                if (result) {
+                    res
+                    .status(200)
+                    .json(result)
+                } else {
+                    res.send(JSON.stringify({
+                        error : 'Error'
+                    }))
+                }
+            }
+        ) 
 };
