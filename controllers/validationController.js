7c8a17f5ce3287f6037975fe930223df60082c56
Ai Nishikawa <60405643+nishi-ai@users.noreply.github.com>
2021-10-04 21:19:04 +0200

    added a middleware for validation password to admin/players

--- controllers/validationController.js
+++ controllers/validationController.js
@@ -1,4 +1,6 @@
 const { body, validationResult } = require('express-validator');
+const url = require('url');
+const querystring = require('querystring');
 
 exports.validateNameAndEmail = [
     // name is required at least 2 chars long
@@ -25,3 +27,36 @@ exports.validateNameAndEmail = [
     next();
     },
 ];
+
+exports.validateAdminPassword = (req, res, next) => {
+    if (process.env.NODE_ENV === 'production') {
+        console.log("It's in production_admin" );
+        adminPassword = process.env.ADMIN_PASSWORD;
+        console.log('adminPassword', adminPassword);
+        rawUrl = `https://softball-register-web.herokuapp.com/admin/players/?password=${process.env.ADMIN_PASSWORD}`
+    } else {
+        console.log("It's in dev_admin");
+        adminPassword = 'fakepassword'
+        console.log('adminPassword', adminPassword);
+        rawUrl = `http://localhost:7000/admin/players/?password=${adminPassword}`
+    }
+
+    const parsedUrl = url.parse(rawUrl);
+    const parsedQs = querystring.parse(parsedUrl.query);
+    console.log("parsedUrl", parsedUrl);
+    console.log("parsedQs", parsedQs);
+
+    const password = req.query.password
+    console.log("password", password);
+    if (password === adminPassword) {
+        // then good to go to players  
+        next();
+    } else {
+        res
+        .status(403)
+        .send(JSON.stringify({
+        error: 'Enter a valid password'
+        }));
+    }
+    
+}
