e9d1c66e37b902d6f1de5e34c68ac579de415ac7
Ai Nishikawa <60405643+nishi-ai@users.noreply.github.com>
2021-10-16 16:25:48 +0200

    added events controller

---
+++ controllers/eventsIndexController.js
@@ -0,0 +1,28 @@
+const db = require('../db');
+
+exports.getEventsIndex = (req, res, next) => {
+    console.log("GET: Hello getEvents")
+    db.getDb()
+        .db('softball')
+        .collection('events')
+        .find(
+            {},
+            {_id: 0, date: 1, result: 1}
+        )
+        .toArray(
+            function(err, result) {
+                if (result) {
+                    res
+                    .status(200)
+                    .json(result)
+                } else {
+                    res
+                    .status(500)
+                    .send(JSON.stringify({
+                        error: 'db-events-find-error',
+                        message: err
+                    }));
+                }
+            }
+        ) 
+};
--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const validationController = require('../controllers/validationController')
 const indexController = require('../controllers/playersIndexController');
+const eventsIndexController = require('../controllers/eventsIndexController');
 
 const router = express.Router();
 
@@ -9,5 +10,8 @@ const router = express.Router();
 // /admin/players => GET
 // validate a password to get players infos
 router.get('/players/', validationController.validateAdminPassword, indexController.getIndex);
+// Get all events info
+// /admin/events => GET
+router.get('/events/', eventsIndexController.getEventsIndex);
 
 module.exports = router;
