8f7c4074158aca4554d898bb88cf9d9ef7e0f335
nishi-ai <aiaimforworld@gmail.com>
2022-04-16 20:23:00 +0200

    added query param

--- controllers/eventsIndexController.js
+++ controllers/eventsIndexController.js
@@ -1,26 +1,20 @@
-const db = require('../db');
+const db = require("../db");
 
 exports.getEventsIndex = async (req, res) => {
-    console.log("GET: Hello getEvents")
-    try { 
-        const collectionEvents = db.getDb().db('softball').collection('events')
-        const allEvents = await collectionEvents.find(
-            {},
-            {_id: 0, date: 1, result: 1}
-        )
-        .sort({ date: -1 })
-        .toArray()
-        console.log('allEvents', allEvents)
-        res
-          .status(200)
-          .json(allEvents);
-    } catch(err) {
-        console.log(err)
-        res
-          .status(500)
-          .json({
-            error: 'db-events-could-not-find',
-            message: err
-        });
-    }
+  console.log("GET: Hello getEvents");
+  const SortedOrder = (req.query.sort = "desc" ? -1 : 1);
+  try {
+    const collectionEvents = db.getDb().db("softball").collection("events");
+    const allEvents = await collectionEvents
+      .find({}, { _id: 0, date: 1, result: 1 })
+      .sort({ date: `${SortedOrder}` })
+      .toArray();
+    res.status(200).json(allEvents);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      error: "db-events-could-not-find",
+      message: err,
+    });
+  }
 };
