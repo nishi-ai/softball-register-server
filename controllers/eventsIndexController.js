52a9ce99e897ae3a9b9a85cce957c650f14db784
nishi-ai <aiaimforworld@gmail.com>
2022-04-17 13:40:51 +0200

    added another two equals to make boolean correct

--- controllers/eventsIndexController.js
+++ controllers/eventsIndexController.js
@@ -2,12 +2,13 @@ const db = require("../db");
 
 exports.getEventsIndex = async (req, res) => {
   console.log("GET: Hello getEvents");
-  const SortedOrder = (req.query.sort = "desc" ? -1 : 1);
+  const sortedOrder = req.query.sort === "asc" ? 1 : -1;
+
   try {
     const collectionEvents = db.getDb().db("softball").collection("events");
     const allEvents = await collectionEvents
       .find({}, { _id: 0, date: 1, result: 1 })
-      .sort({ date: `${SortedOrder}` })
+      .sort({ date: sortedOrder })
       .toArray();
     res.status(200).json(allEvents);
   } catch (err) {
